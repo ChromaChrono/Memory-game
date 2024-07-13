4a2f1408afe2ed818b68b8b0bd72d1e3d5891495
unknown <ideaswrittenonleaves@gmail.com>
2024-07-13 14:22:01 +0200

    Skeleton created

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import startScreen from "./components/start-screen.vue"
+</script>
+
+<template>
+<h1>Jawayriya is the best</h1>
+<startScreen></startScreen>
+</template>
+
+<style scoped>
+</style>
+
