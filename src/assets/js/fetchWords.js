20af116a43c0ad0ec78438f54be91a43be1a05fa
unknown <ideaswrittenonleaves@gmail.com>
2024-07-15 21:13:23 +0100

    small changes

--- src/assets/js/fetchWords.js
+++ src/assets/js/fetchWords.js
@@ -1,16 +1,15 @@
-import words from "../words.json"
+import words from "../words.json";
 
 const randomNumberGenerator = (max = 1489) => {
-return Math.floor(Math.random() * max)
-}
+    return Math.floor(Math.random() * max);
+};
 
 export default async (num) => {
-    const newWordArray = []
+    const newWordArray = [];
 
-    for (let i = 0; i<num; i++) {
-        newWordArray.push(words.words[randomNumberGenerator()])
+    for (let i = 0; i < num; i++) {
+        newWordArray.push(words.words[randomNumberGenerator()]);
     }
 
-    return newWordArray
-
-}
+    return newWordArray;
+};
