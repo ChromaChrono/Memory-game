55eea2d39deab64a5dcae318f789c1424531951c
unknown <ideaswrittenonleaves@gmail.com>
2024-08-17 18:25:54 +0100

    added build rollup options to vite config

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,9 @@ export default defineConfig({
     test: {
         environment: "jsdom",
     },
+    build: {
+        rollupOptions: {
+            external: ["vue-router"],
+        },
+    },
 });
